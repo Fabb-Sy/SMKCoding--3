6c71bbe18a549e6e7ab474a88d4d955d8115b1b2
DikoPoral <fadhlishabri416@gmail.com>
2023-11-16 23:08:14 +0700

    Tambahkan proyek SMKCODING--6

--- ecommerce/components/forms/CategoryForm.vue
+++ ecommerce/components/forms/CategoryForm.vue
@@ -1,27 +1,9 @@
-<script lang="ts" setup>
-  import { category } from "~/composables/constants/category";
-  const categories = ref(category);
-  const form = ref({
-    name: "",
-  })
-  const isShowAlert = ref(false);
-
-  const submitCategory = () => {
-    categories.value.push({ id: categories.value.length + 1, name: form.value.name });
-    isShowAlert.value = true;
-    form.value.name = "";
-    setTimeout(() => {
-      isShowAlert.value = false;
-    }, 3000);
-  }
-</script>
-
 <template>
   <section class="flex justify-center py-10">
     <div class="w-[500px]">
       <h1 class="text-2xl mb-7 font-medium">Create Category</h1>
       <div v-if="isShowAlert" class="p-4 mb-4 text-sm rounded-lg bg-green-100 text-green-800">
-        Create Category Successfully
+        Category "{{ form.name }}" created successfully!
       </div>
       <form @submit.prevent="submitCategory">
         <div class="mb-6">
@@ -31,9 +13,9 @@
             id="name"
             v-model="form.name"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            placeholder="Masukan Nama Category"
+            placeholder="Masukkan Nama Category"
             required="true"
-          >
+          />
         </div>
         <button
           type="submit"
@@ -45,3 +27,29 @@
     </div>
   </section>
 </template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue';
+  import { useCategoryStore } from '~/stores/category';
+
+  const categoryStore = useCategoryStore();
+  const form = ref({
+    name: '',
+  });
+  const isShowAlert = ref(false);
+
+  const submitCategory = async () => {
+    try {
+      await categoryStore.createCategory(form.value);
+      isShowAlert.value = true;
+
+      setTimeout(() => {
+        isShowAlert.value = false;
+        form.value.name = ''; // Clear the input after successful submission
+      }, 3000);
+    } catch (error) {
+      console.error('Error submitting category:', error);
+      // Handle error (e.g., show an error alert)
+    }
+  };
+</script>
